b6fd8858cf4716eb553a04e2ae951c2995c07a36
Alejandro Villegas <alejandrocvillegas@gmail.com>
2023-02-22 17:55:33 +0100

    main

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+const cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json())
 morgan.token("payload", function (req) {
   return JSON.stringify(req.body)
@@ -104,7 +108,7 @@ response.json(person)
   })
 
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
